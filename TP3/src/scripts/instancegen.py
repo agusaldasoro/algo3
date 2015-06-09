2fef4230400107ba25c1a10c1a44c88b706c995f
Francisco <frannoriega.92@gmail.com>
2015-06-09 20:00:34 -0300

    Miren el instancegen, esta re copado

--- TP3/src/scripts/instancegen.py
+++ TP3/src/scripts/instancegen.py
@@ -8,31 +8,38 @@ def instancegen():
 			if(execution.lower() == "once"):
 				try:
 					nodes = input("Please enter the amount of nodes: ")
-					if(not(type(nodes) is int)):
-						raise NameError()
 					while True:
 						try:
-							maxNodes = (nodes*(nodes-1)/2)
-							edges = input("Please enter the amount of edges (MAX: "+str(maxNodes)+"): ")
-							if(edges > maxNodes):
+							maxEdges = (nodes*(nodes-1)/2)
+							edges = input("Please enter the amount of edges (MAX: "+str(maxEdges)+"): ")
+							if(edges > maxEdges):
 								raise NameError()
 							break
 						except NameError:
-							print "Too many edges. There must be less than "+str(maxNodes)
+							print "Too many edges. There must be less than "+str(maxEdges)
 					break
 	 			except NameError:
 	 				print "Invalid syntax. An integer must be provided"	
 	 		elif(execution.lower() == "several"):
 	 			try:
 	 				tests = input("Please enter the amount of tests: ")
-	 				if(not(type(tests) is int)):
-						raise NameError()
-					nodeRange = input("Enter the maximum amount of nodes: ")
-					if(not(type(nodeRange) is int)):
-						raise NameError()
-	 				break
+	 				fix = raw_input("What element would you have fixed? (Nodes/Edges): ")
+	 				if(fix.lower() == "nodes"):
+	 					nodes = input("Enter the amount of nodes: ")
+	 					maxEdges = (nodes*(nodes-1)/2)
+	 					edges = input("Enter the starting edges (MAX: "+str(maxEdges)+"): ")
+	 					step = input("Enter the steps: ")
+	 					break
+	 				elif(fix.lower() == "edges"):
+	 					nodes = input("Enter the starting nodes: ")
+	 					maxEdges = (nodes*(nodes-1)/2)
+	 					edges = input("Enter the amount of edges (MAX: "+str(maxEdges)+"): ")
+	 					step = input("Enter the steps: ")
+	 					break
+	 				else:
+	 					raise NameError()
 	 			except NameError:
-	 				print "Invalid syntax. An integer must be provided"
+	 				print "Invalid syntax or value."
 	 		else:
 	 			raise NameError()
 	 	except NameError:
@@ -63,33 +70,58 @@ def instancegen():
 			f.write(str(fromNode)+' '+str(toNode)+'\n')
 		f.close()
 	else:
-		for test in range(0, tests):
-			nodes = r.randint(1,nodeRange)
-			trivial = 0
-			if(nodes > 1):
-				trivial = 1
-			edges = r.randint(trivial,(nodes*(nodes-1))/2)
-			filename = "Instance-"+str(nodes)+"-"+str(edges)+".txt"
-			f = open(filename, 'w')
-			f.write(str(nodes)+' '+str(edges)+'\n')
-			nodeList = [node for node in range(0,nodes)]
-			edgesUsed = []
-			for edge in range(0, edges):
-				fromNode = r.choice(nodeList)
-				nodeList.remove(fromNode)
-				toNode = r.choice(nodeList)
-				nodeList.append(fromNode)
-				choice1 = [fromNode, toNode]
-				choice2 = [toNode, fromNode]
-				while ((edgesUsed.count(choice1) > 0) or (edgesUsed.count(choice2) > 0)):
+		test = 0
+		if(fix.lower() == "nodes"):
+			while(test < tests and edges < maxEdges):
+				filename = "Instance-"+str(nodes)+"-"+str(edges)+".txt"
+				f = open(filename, 'w')
+				f.write(str(nodes)+' '+str(edges)+'\n')
+				nodeList = [node for node in range(0,nodes)]
+				edgesUsed = []
+				for edge in range(0, edges):
 					fromNode = r.choice(nodeList)
 					nodeList.remove(fromNode)
 					toNode = r.choice(nodeList)
 					nodeList.append(fromNode)
 					choice1 = [fromNode, toNode]
 					choice2 = [toNode, fromNode]
-				edgesUsed.append(choice1)
-				f.write(str(fromNode)+' '+str(toNode)+'\n')
-			f.close()
+					while ((edgesUsed.count(choice1) > 0) or (edgesUsed.count(choice2) > 0)):
+						fromNode = r.choice(nodeList)
+						nodeList.remove(fromNode)
+						toNode = r.choice(nodeList)
+						nodeList.append(fromNode)
+						choice1 = [fromNode, toNode]
+						choice2 = [toNode, fromNode]
+					edgesUsed.append(choice1)
+					f.write(str(fromNode)+' '+str(toNode)+'\n')
+				f.close()
+				edges+=step
+				test+=1
+		else:
+			while(test < tests):
+				filename = "Instance-"+str(nodes)+"-"+str(edges)+".txt"
+				f = open(filename, 'w')
+				f.write(str(nodes)+' '+str(edges)+'\n')
+				nodeList = [node for node in range(0,nodes)]
+				edgesUsed = []
+				for edge in range(0, edges):
+					fromNode = r.choice(nodeList)
+					nodeList.remove(fromNode)
+					toNode = r.choice(nodeList)
+					nodeList.append(fromNode)
+					choice1 = [fromNode, toNode]
+					choice2 = [toNode, fromNode]
+					while ((edgesUsed.count(choice1) > 0) or (edgesUsed.count(choice2) > 0)):
+						fromNode = r.choice(nodeList)
+						nodeList.remove(fromNode)
+						toNode = r.choice(nodeList)
+						nodeList.append(fromNode)
+						choice1 = [fromNode, toNode]
+						choice2 = [toNode, fromNode]
+					edgesUsed.append(choice1)
+					f.write(str(fromNode)+' '+str(toNode)+'\n')
+				f.close()
+				nodes+=step
+				test+=1
 
 instancegen()
