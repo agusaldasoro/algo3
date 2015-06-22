9daeae19fb5980d3f71d461020cb385e1a6cfeed
Agustina <agusaldasoro@gmail.com>
2015-06-22 01:45:39 -0300

    subo resultados parciales

--- TP3/src/instanciasAgustina/script.sh
+++
@@ -1,9 +0,0 @@
-for i in {1..100}
-do
-	cat instanciasAgustina/grafosCompletos/Instance-1-0.txt | ./main 2 >> Instance-1-0V$i.out
- done
-# for i in {10..26}
-# do
-# 	cat tests.tp1.ej2/0$i.in | ./main
-# done
-
